4182e316242a12db908420e671b3d1882a054f0b
Irkbirkani <aturner3@trinity.edu>
2019-11-16 14:04:28 -0600

    Added assets

---
+++ Dungeon Arena/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(horz, 0, vert);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + tempVect);
+
+    }
+}
